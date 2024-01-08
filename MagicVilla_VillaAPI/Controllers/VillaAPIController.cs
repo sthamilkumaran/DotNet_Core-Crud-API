f5ee0f753a529aa47c614a75b881d042d3bb2321
Selvarasha Thamilkkumaran <balapriyanka70@gmail.com>
2024-01-08 20:05:41 +0530

    get

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Tamil" },
+                new Villa { Id = 2, Name = "Kumaran" },
+            };
+        }
+    }
+}
