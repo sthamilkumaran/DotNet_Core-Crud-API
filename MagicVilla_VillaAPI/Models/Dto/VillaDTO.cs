bd8bb4f71c978b8d1d2a9e8c88d5cea44359c154
Selvarasha Thamilkkumaran <balapriyanka70@gmail.com>
2024-01-09 15:39:28 +0530

    Mssql Data base setup

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -8,8 +8,13 @@ namespace MagicVilla_VillaAPI.Models.Dto
         [Required]
         [MaxLength(30)]
         public string Name { get; set; }
+        public string Details {  get; set; }
+        [Required]
+        public double Rate {  get; set; }
         public int Occupancy { get; set; }
         public int Sqtf {  get; set; }
+        public string ImageUrl {  get; set; }
+        public string Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
     }
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,10 +1,16 @@
 
 
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Logging;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
 builder.Services.AddControllers(option =>
 {
